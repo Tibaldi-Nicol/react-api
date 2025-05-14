7c45331b4d30a7f6478533e8e4a2f617dbdccc39
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-05-14 16:11:27 +0200

    [UPD]Add-Actor-card

---
+++ src/ActorCard.jsx
@@ -0,0 +1,17 @@
+//qui creo la card degli attori poi con map in app.jsx ne ricrera una per ognuno
+
+function ActorCard({name, birthyear, nationality, image, bio, awards}){
+    <div className="Actor-Card">
+
+        <img src={image} alt={name} />
+        <p><strong>Nato nel:</strong>{birthyear}</p>
+        <p><strong>Nazionalità:</strong> {nationality}</p>
+        <p><strong>Biografia:</strong> {bio}</p>
+        <p><strong>Premi:</strong> {awards}</p>
+
+
+
+    </div>
+}
+
+export default ActorCard;
