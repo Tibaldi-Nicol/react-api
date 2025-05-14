52876839add35467d48d9ffce59e8373fb2f7c47
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-05-14 13:06:48 +0200

    [UPD]Add-milestone-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-
-import './App.css'
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    axios.get("https://lanciweb.github.io/demo/api/actresses/")
+      .then(res => {
+        console.log("Dati ricevuti:", res.data);
+      });
+  }, []);
 
   return (
-
+    <div>
+      <h1>Cast Fetching</h1>
+      <p>Apri la console (F12) per vedere i dati</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
